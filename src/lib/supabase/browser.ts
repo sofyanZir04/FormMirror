5074b4370abd08e6387a65e3843966d641478ef1
Sofyane <sofyan04Ozou@hotmail.com>
2025-11-12 23:08:10 +0100

    Update Settings

--- src/lib/supabase/browser.ts
+++ src/lib/supabase/browser.ts
@@ -1,5 +1,7 @@
 import { createBrowserClient } from '@supabase/ssr'
-import { Database } from '@/types/supabase'
+// import { Database } from '@/types/supabase'
+import { Database } from '../../types/database'
+
 
 export const supabase = createBrowserClient<Database>(
   process.env.NEXT_PUBLIC_SUPABASE_URL!,
